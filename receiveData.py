77d9645b89bb771c611a3ede0ab3b72832b52920
mjwheelemtu <mjwheele@mtu.edu>
2019-03-04 15:59:39 -0500

    adds a python file with the beginnings of a function to receive data from the app to the sim

---
+++ receiveData.py
@@ -0,0 +1,32 @@
+#Example input BC:7-CS:3-.....
+#BC - Charge Capacity
+#CS - Charge Speed
+#CU - Charge Update
+#CR - Charge Request
+#PR - Port Request
+def appToSim(inputString):
+	#inputString = "BC:7.235-CS:5.322-"
+	
+	commandList = inputString.split("-")  #Split input string into commands
+	commandList = commandList[:-1]  #Remove empty command resulting in last "-"
+	
+	command = "" #storage for individual command from Command List
+	
+	# identifier="" #Identifier from a given command ex: "BC"
+	
+	commIDPair = {}
+	
+	value = 0     #Float value of the command
+	
+	
+	for cmd in commandList:
+		command = cmd.split(":")
+		commIDPair.update({ command[0] : float(command[1]) })
+		print(commIDPair[command[0]])
+		
+		
+
+
+appToSim("BC:7.235-CS:5.322-")
+
+
