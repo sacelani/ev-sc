debf39a109acc20e67b13d048116d3102cf54697
mjwheelemtu <mjwheele@mtu.edu>
2019-03-06 16:25:48 -0500

    updated receiveData.py

--- receiveData.py
+++ receiveData.py
@@ -5,9 +5,9 @@
 #CR - Charge Request
 #PR - Port Request
 def appToSim(inputString):
-	#inputString = "BC:7.235-CS:5.322-"
-	
+	#inputString = "-BC:7.235-CS:5.322-"
 	commandList = inputString.split("-")  #Split input string into commands
+	commandList = commandList[1:]	
 	commandList = commandList[:-1]  #Remove empty command resulting in last "-"
 	
 	command = "" #storage for individual command from Command List
@@ -22,11 +22,22 @@ def appToSim(inputString):
 	for cmd in commandList:
 		command = cmd.split(":")
 		commIDPair.update({ command[0] : float(command[1]) })
-		print(commIDPair[command[0]])
+		#print(commIDPair[command[0]])
 		
-		
+	for cmd in commIDPair:
+		if (cmd == "BC"):
+			ChargeCapcity = commIDPair["BC"]
+		if (cmd == "CS"):
+			ChargeSpeed = commIDPair["CS"]
+		if (cmd == "CU"):
+			ChargeUpdage = commIDPair["CU"]
+		if (cmd == "CR"):
+			ChargeRequest = commIDPair["CR"]
+		if (cmd == "PR"):
+			PortRequest = commIDPair["PR"]
 
 
-appToSim("BC:7.235-CS:5.322-")
+
+appToSim("-BC:7.235-CS:5.322-")
 
 
